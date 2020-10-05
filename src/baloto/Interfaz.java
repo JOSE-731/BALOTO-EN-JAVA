1af43884dc16d953d5832b59ed6441015a123dc7
Adrian <adjodero81@gmail.com>
2020-10-04 19:16:22 -0500

    Comentario parte1

--- src/baloto/Interfaz.java
+++ src/baloto/Interfaz.java
@@ -427,7 +427,7 @@ public class Interfaz extends javax.swing.JFrame {
         resbol6.setText(Numeroganador[5] + "");
         
         
-        //Hacemos las condiciones  a los numeros que ingresa el usuario
+        //Hacemos las condiciones  a los numeros que ingresa el usuarios
         if (numero_usuario[0] >= 0 && numero_usuario[0] <= 99 && numero_usuario[1] >= 0 && numero_usuario[1] <= 99 && numero_usuario[2] >= 0 && numero_usuario[2] <= 99 && numero_usuario[3] >= 0 && numero_usuario[3] <= 99 && numero_usuario[4] >= 0 && numero_usuario[4] <= 99 && numero_usuario[5] >= 0 && numero_usuario[5] <= 99) {
             
             if (numero_usuario[0] == Numeroganador[0] && numero_usuario[1] == Numeroganador[1] && numero_usuario[2] == Numeroganador[2] && numero_usuario[3] == Numeroganador[3] && numero_usuario[4] == Numeroganador[4] && numero_usuario[5] == Numeroganador[5]) {
