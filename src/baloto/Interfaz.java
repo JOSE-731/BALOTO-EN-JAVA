ba77d2c6ece9345cc1e417b90d52a5be5f61c7ad
Adrian <adjodero81@gmail.com>
2020-10-04 20:12:37 -0500

    mensaje1.2

--- src/baloto/Interfaz.java
+++ src/baloto/Interfaz.java
@@ -446,7 +446,7 @@ public class Interfaz extends javax.swing.JFrame {
                 premio.setText("Felicidades, usted ganó: $"+pago1);
                 premio.setText("Acertó en 3 numeros");
             }else if(numero_usuario[0] == Numeroganador[0] && numero_usuario[1] == Numeroganador[1] && numero_usuario[2] == Numeroganador[2] && numero_usuario[3] == Numeroganador[3] && numero_usuario[4] == Numeroganador[4]){
-                
+                 premio.setText("Felicidades, usted ganó: $"+pago2);
             }
 
         } else {
